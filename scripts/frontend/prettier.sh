ea11a7f63f7a840ee4b6b5f755d06fbbc101f5cb
Lukas Eipert <git@leipert.io>
2018-03-29 13:50:22 +0200

    rewrite prettier script with bash

---
+++ scripts/frontend/prettier.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+set -o noglob
+
+: ${1?"Usage: $0 <check|save|check-all|save-all>"}
+
+PRETTIER_PATH=$(git rev-parse --show-toplevel)/node_modules/.bin/prettier
+
+if [ ! -f $PRETTIER_PATH ]; then
+    echo "ERROR: Prettier not found!"
+    exit 1
+fi
+
+case $1 in
+check)
+  action=check
+  prettier_args=--list-different
+  files=staged
+  ;;
+save)
+  action=save
+  prettier_args=--write
+  files=staged
+  ;;
+check-all)
+  action=check
+  prettier_args=--list-different
+  files=all
+  ;;
+save-all)
+  action=save
+  prettier_args=--write
+  files=all
+  ;;
+*)
+  echo "$1 is not a valid argument, please use check, save, check-all or save-all"
+  exit 1
+  ;;
+esac
+
+echo "Loading $files files ..."
+
+if [ "$files" == "staged" ]; then
+    file_list=$(git diff --name-only --cached --diff-filter=ACMRTUB '*.vue' '*.js' '*.scss'  | tr '\r\n' ' ')
+else
+    file_list="**/*.vue **/*.js **/*.scss"
+fi
+
+if [ "$action" == "check" ]; then
+    echo -e "Running command: prettier $prettier_args $file_list\n"
+    if $PRETTIER_PATH $prettier_args $file_list; then
+        echo -e "\nFormat of $files files is correct."
+        exit 0
+    else
+        echo -e "\nPlease format the files listed above."
+        exit 1
+    fi
+fi
+
+if [ "$action" == "save" ]; then
+    echo -e "Running command: prettier $prettier_args $file_list\n"
+    if $PRETTIER_PATH $prettier_args $file_list; then
+        echo -e "\nFormatted $files files successfully with prettier."
+        exit 0
+    else
+        echo -e "\nSomething went wrong while formatting with prettier."
+        exit 1
+    fi
+fi
+
+echo "ERROR: Something went wrong"
+
+exit 1
