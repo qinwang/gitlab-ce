42ec60e8a74478c62962f5c66abfbe3e908d9e49
Shinya Maeda <shinya@gitlab.com>
2018-07-02 14:46:38 +0900

    Unify partial index migrations

--- db/post_migrate/20180430161409_migrate_legacy_artifacts_to_job_artifacts.rb
+++ db/post_migrate/20180430161409_migrate_legacy_artifacts_to_job_artifacts.rb
@@ -4,6 +4,7 @@ class MigrateLegacyArtifactsToJobArtifacts < ActiveRecord::Migration
   DOWNTIME = false
   MIGRATION = 'MigrateLegacyArtifacts'.freeze
   BATCH_SIZE = 100
+  TMP_INDEX = 'tmp_partial_index_ci_builds_on_id_with_legacy_artifacts'.freeze
 
   disable_ddl_transaction!
 
@@ -17,6 +18,10 @@ class MigrateLegacyArtifactsToJobArtifacts < ActiveRecord::Migration
   end
 
   def up
+    unless index_exists_by_name?(:ci_builds, TMP_INDEX)
+      add_concurrent_index(:ci_builds, :id, where: "artifacts_file <> ''", name: TMP_INDEX)
+    end
+
     MigrateLegacyArtifactsToJobArtifacts::Build
       .with_legacy_artifacts.tap do |relation|
       queue_background_migration_jobs_by_range_at_intervals(relation,
@@ -27,6 +32,8 @@ class MigrateLegacyArtifactsToJobArtifacts < ActiveRecord::Migration
   end
 
   def down
-    # no-op
+    if index_exists_by_name?(:ci_builds, TMP_INDEX)
+      remove_concurrent_index_by_name(:ci_builds, TMP_INDEX)
+    end
   end
 end
--- db/post_migrate/20180702135109_fill_file_location_to_job_artifacts.rb
+++ db/post_migrate/20180702135109_fill_file_location_to_job_artifacts.rb
@@ -2,29 +2,32 @@ class FillFileLocationToJobArtifacts < ActiveRecord::Migration
   include Gitlab::Database::MigrationHelpers
 
   DOWNTIME = false
-  MIGRATION = 'MigrateLegacyArtifacts'.freeze
   BATCH_SIZE = 10_000
+  TMP_INDEX = 'tmp_index_ci_job_artifacts_on_id_with_null_file_location'.freeze
+  CI_JOB_ARTIFACT_FILE_LOCATION_HASHED_PATH = 2 # Equavalant to Ci::JobArtifact.file_locations[:hashed_path]
 
   disable_ddl_transaction!
-
-  CI_JOB_ARTIFACT_FILE_LOCATION_HASHED_PATH = 2 # Equavalant to Ci::JobArtifact.file_locations[:hashed_path]
 
   class JobArtifact < ActiveRecord::Base
     include EachBatch
     self.table_name = 'ci_job_artifacts'
-
-    def self.params_for_background_migration
-      yield self.where(file_store: nil), 'FillFileStoreJobArtifact', 5.minutes, BATCH_SIZE
-    end
   end
 
   def up
+    unless index_exists_by_name?(:ci_job_artifacts, TMP_INDEX)
+      # This partial index is to be removed after the clean-up phase of the background migrations for legacy artifacts.
+      add_concurrent_index(:ci_job_artifacts, :id, where: 'file_location is NULL', name: TMP_INDEX)
+    end
+
+    # TODO: Use background migrations?
     FillFileLocationToJobArtifacts::JobArtifact.where(file_location: nil).each_batch(of: BATCH_SIZE) do |relation|
       relation.update_all(file_location: CI_JOB_ARTIFACT_FILE_LOCATION_HASHED_PATH)
     end
   end
 
   def down
-    # no-op
+    if index_exists_by_name?(:ci_job_artifacts, TMP_INDEX)
+      remove_concurrent_index_by_name(:ci_job_artifacts, TMP_INDEX)
+    end
   end
 end
