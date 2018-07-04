469e826429feb6a8ce66eb5d5e087f7182eb8813
Shinya Maeda <shinya@gitlab.com>
2018-07-04 19:09:12 +0900

    Clean up migrate_legacy_artifacts spec

--- spec/lib/gitlab/background_migration/migrate_legacy_artifacts_spec.rb
+++ spec/lib/gitlab/background_migration/migrate_legacy_artifacts_spec.rb
@@ -7,30 +7,32 @@ describe Gitlab::BackgroundMigration::MigrateLegacyArtifacts, :migration, schema
   let(:jobs) { table(:ci_builds) }
   let(:job_artifacts) { table(:ci_job_artifacts) }
 
-  context 'when a pipeline exists' do
-    let(:pipeline_id) { 1 }
-    let(:project_id) { 123 }
+  subject { described_class.new.perform(*range) }
 
-    before do
-      namespaces.create!(id: 1, name: 'gitlab', path: 'gitlab-org')
-      projects.create!(id: project_id, name: 'gitlab', path: 'gitlab-ce', namespace_id: 1)
-      pipelines.create!(id: pipeline_id, project_id: project_id, ref: 'master', sha: 'adf43c3a')
-    end
+  context 'when a pipeline exists' do
+    let!(:namespace) { namespaces.create!(name: 'gitlab', path: 'gitlab-org') }
+    let!(:project) { projects.create!(name: 'gitlab', path: 'gitlab-ce', namespace_id: namespace.id) }
+    let!(:pipeline) { pipelines.create!(project_id: project.id, ref: 'master', sha: 'adf43c3a') }
 
     context 'when a legacy artifacts exists' do
       let(:artifacts_expire_at) { 1.day.since.to_s }
-      let(:file_store) { 2 }
-      let(:job_id) { 1 }
-      let(:file_type_archive) { 1 }
-      let(:file_type_metadata) { 2 }
-      let(:file_location_legacy_path) { 1 }
+      let(:file_store) { ::ObjectStorage::Store::REMOTE }
+
+      let!(:job) do
+        jobs.create!(
+          commit_id: pipeline.id,
+          project_id: project.id,
+          status: :success,
+          **artifacts_archive_attributes,
+          **artifacts_metadata_attributes)
+      end
 
       let(:artifacts_archive_attributes) do
         {
           artifacts_file: 'archive.zip',
           artifacts_file_store: file_store,
           artifacts_size: 123,
-          artifacts_expire_at: artifacts_expire_at
+          artifacts_expire_at: 1.day.since.to_s
         }
       end
 
@@ -39,10 +41,6 @@ describe Gitlab::BackgroundMigration::MigrateLegacyArtifacts, :migration, schema
           artifacts_metadata: 'metadata.gz',
           artifacts_metadata_store: file_store
         }
-      end
-
-      before do
-        jobs.create!(id: job_id, commit_id: pipeline_id, project_id: project_id, status: :success, **artifacts_archive_attributes, **artifacts_metadata_attributes)
       end
 
       it 'has legacy artifacts' do
@@ -55,14 +53,14 @@ describe Gitlab::BackgroundMigration::MigrateLegacyArtifacts, :migration, schema
       end
 
       context 'when the record exists inside of the range of a background migration' do
-        let(:range) { [1, 1] }
+        let(:range) { [job.id, job.id] }
 
-        it 'migrates' do
-          described_class.new.perform(*range)
+        it 'migrates a legacy artifact to ci_job_artifacts table' do
+          expect { subject }.to change { job_artifacts.count }.by(2)
 
           expect(job_artifacts.order(:id).pluck('project_id, job_id, file_type, file_store, size, expire_at, file, file_sha256, file_location'))
-            .to eq([[project_id, job_id, file_type_archive,  file_store, artifacts_archive_attributes[:artifacts_size], artifacts_expire_at, 'archive.zip', nil, file_location_legacy_path],
-                    [project_id, job_id, file_type_metadata, file_store,                                           nil, artifacts_expire_at, 'metadata.gz', nil, file_location_legacy_path]])
+            .to eq([[project.id, job.id, described_class::ARCHIVE_FILE_TYPE,  file_store, artifacts_archive_attributes[:artifacts_size], artifacts_expire_at, 'archive.zip', nil, described_class::LEGACY_PATH_FILE_LOCATION],
+                    [project.id, job.id, described_class::METADATA_FILE_TYPE, file_store,                                           nil, artifacts_expire_at, 'metadata.gz', nil, described_class::LEGACY_PATH_FILE_LOCATION]])
 
           expect(jobs.pluck('artifacts_file, artifacts_file_store, artifacts_size, artifacts_expire_at')).to eq([[nil, nil, nil, artifacts_expire_at]])
           expect(jobs.pluck('artifacts_metadata, artifacts_metadata_store')).to eq([[nil, nil]])
@@ -71,69 +69,70 @@ describe Gitlab::BackgroundMigration::MigrateLegacyArtifacts, :migration, schema
         context 'when file_store is nil' do
           let(:file_store) { nil }
 
-          it 'fills file_store by 1 (ObjectStorage::Store::LOCAL)' do
+          it 'has nullified file_store in all legacy artifacts' do
             expect(jobs.pluck('artifacts_file_store, artifacts_metadata_store')).to eq([[nil, nil]])
+          end
 
-            described_class.new.perform(*range)
+          it 'fills file_store by the value of local file store' do
+            subject
 
-            expect(job_artifacts.pluck('file_store')).to eq([1, 1])
+            expect(job_artifacts.pluck('file_store')).to all(eq(::ObjectStorage::Store::LOCAL))
           end
         end
 
         context 'when new artifacts has already existed' do
-          context 'when only archive existed' do
+          context 'when only archive.zip existed' do
             before do
-              job_artifacts.create!(id: 1, project_id: project_id, job_id: job_id, file_type: 1, size: 123, file: 'archive.zip')
+              job_artifacts.create!(project_id: project.id, job_id: job.id, file_type: described_class::ARCHIVE_FILE_TYPE, size: 999, file: 'archive.zip')
             end
 
-            it 'migrates metadata too' do
-              described_class.new.perform(*range)
+            it 'had archive.zip already' do
+              expect(job_artifacts.exists?(job_id: job.id, file_type: described_class::ARCHIVE_FILE_TYPE)).to be_truthy
+            end
 
-              expect(job_artifacts.where(job_id: job_id, file_type: 2).pluck('file')).to eq(['metadata.gz'])
+            it 'migrates metadata' do
+              expect { subject }.to change { job_artifacts.count }.by(1)
+
+              expect(job_artifacts.exists?(job_id: job.id, file_type: described_class::METADATA_FILE_TYPE)).to be_truthy
             end
           end
 
           context 'when both archive and metadata existed' do
             before do
-              job_artifacts.create!(id: 1, project_id: project_id, job_id: job_id, file_type: 1, size: 123, file: 'archive.zip')
-              job_artifacts.create!(id: 2, project_id: project_id, job_id: job_id, file_type: 2, size: 123, file: 'metadata.gz')
+              job_artifacts.create!(project_id: project.id, job_id: job.id, file_type: described_class::ARCHIVE_FILE_TYPE, size: 999, file: 'archive.zip')
+              job_artifacts.create!(project_id: project.id, job_id: job.id, file_type: described_class::METADATA_FILE_TYPE, size: 999, file: 'metadata.zip')
             end
 
             it 'does not migrate' do
-              expect { described_class.new.perform(*range) }.not_to change { job_artifacts.count }
+              expect { subject }.not_to change { job_artifacts.count }
             end
           end
         end
       end
 
       context 'when the record exists outside of the range of a background migration' do
-        let(:range) { [2, 2] }
+        let(:range) { [job.id + 1, job.id + 1] }
 
         it 'does not migrate' do
-          described_class.new.perform(*range)
-
-          expect(job_artifacts.count).to be_zero
+          expect { subject }.not_to change { job_artifacts.count }
         end
       end
     end
 
-    context 'when legacy artifacts do not exist' do
-      before do
-        jobs.create!(id: 1, commit_id: pipeline_id, project_id: project_id, status: :success)
-      end
+    context 'when the job does not have legacy artifacts' do
+      let!(:job) { jobs.create!(commit_id: pipeline.id, project_id: project.id, status: :success) }
 
-      it 'is not found from database' do
+      it 'does not have the legacy artifacts in database' do
+        expect(jobs.count).to eq(1)
         expect(jobs.pluck('artifacts_file, artifacts_file_store, artifacts_size, artifacts_expire_at')).to eq([[nil, nil, nil, nil]])
         expect(jobs.pluck('artifacts_metadata, artifacts_metadata_store')).to eq([[nil, nil]])
       end
 
       context 'when the record exists inside of the range of a background migration' do
-        let(:range) { [1, 1] }
+        let(:range) { [job.id, job.id] }
 
         it 'does not migrate' do
-          described_class.new.perform(*range)
-
-          expect(job_artifacts.count).to be_zero
+          expect { subject }.not_to change { job_artifacts.count }
         end
       end
     end
