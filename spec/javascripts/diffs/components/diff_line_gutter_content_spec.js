856bb0e7d5bb38151172b199a8c9a51758b5069a
Fatih Acet <acetfatih@gmail.com>
2018-07-10 01:21:22 +0200

    Fix failing Karma specs becasue of modulized mutations.

--- spec/javascripts/diffs/components/diff_line_gutter_content_spec.js
+++ spec/javascripts/diffs/components/diff_line_gutter_content_spec.js
@@ -18,12 +18,12 @@ describe('DiffLineGutterContent', () => {
   };
   const setDiscussions = component => {
     component.$store.dispatch('setInitialNotes', getDiscussionsMockData());
-    component.$store.commit('SET_DIFF_DATA', { diffFiles: [getDiffFileMock()] });
+    component.$store.commit('diffs/SET_DIFF_DATA', { diffFiles: [getDiffFileMock()] });
   };
 
   const resetDiscussions = component => {
     component.$store.dispatch('setInitialNotes', []);
-    component.$store.commit('SET_DIFF_DATA', {});
+    component.$store.commit('diffs/SET_DIFF_DATA', {});
   };
 
   describe('computed', () => {
